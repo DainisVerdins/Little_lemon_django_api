ef351430a02106de8addf30d9de82e0f8b5efbb1
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2023-04-27 21:50:11 +0300

    feat: fully functional menu_item endpoint

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -3,6 +3,7 @@ from . import views
 from rest_framework.authtoken.views import obtain_auth_token
   
 urlpatterns = [ 
-    path('menu-items', views.MenuItemList.as_view()), 
-    path('api-token-auth/', obtain_auth_token)
+    path('menu-items', views.menu_items, name='menu-items'), 
+    path('api-token-auth/', obtain_auth_token),
+    path('menu-items/<int:pk>', views.menu_item, name='menu-item')
 ] 
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -3,27 +3,25 @@ from .models import MenuItem, Category
 from .serializers import MenuItemSerializer
 from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework import status
 
 # Create your views here.
-
-class MenuItemList(generics.ListCreateAPIView):
+@api_view(['GET','POST'])
+def menu_items(request):
     """
     View list of menu items in the system.
 
     * Only Manager user can create new menu items.
     """
 
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
-
-    def get(self, request):
+    if request.method == 'GET':
         items = MenuItem.objects.all()
         serialized_item = MenuItemSerializer(items, many=True)
+        
         return Response(serialized_item.data, status.HTTP_200_OK)
     
-    def post(self, request):
+    elif request.method == 'POST':
         if request.user.groups.filter(name="Manager").exists():
             title = request.POST.get('title')
             price = request.POST.get('price')
@@ -46,3 +44,41 @@ class MenuItemList(generics.ListCreateAPIView):
 
         else:
             return Response({"message": "this operation is permited!"}, status.HTTP_403_FORBIDDEN)
+
+@api_view(['GET','PUT','PATCH','DELETE'])
+def menu_item(request, pk):
+
+    try:
+        item = MenuItem.objects.get(pk=pk)
+    except MenuItem.DoesNotExist:
+        return Response(status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serialized_item = MenuItemSerializer(item, many=False)
+        
+        return Response(serialized_item.data, status.HTTP_200_OK)
+    
+    if request.method == 'PUT':
+        if request.user.groups.filter(name="Manager").exists():
+            serializer = MenuItemSerializer(item, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(status.HTTP_200_OK)
+        
+        return Response({"message": "this operation is permited!"}, status.HTTP_403_FORBIDDEN)
+    
+    if request.method == 'PATCH':
+        if request.user.groups.filter(name="Manager").exists():
+            serializer = MenuItemSerializer(item, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(status.HTTP_200_OK)
+        
+        return Response({"message": "this operation is permited!"}, status.HTTP_403_FORBIDDEN)
+    
+    if request.method == 'DELETE':
+        if request.user.groups.filter(name="Manager").exists():
+            item.delete()
+            return Response(serialized_item.data, status.HTTP_200_OK)
+        
+        return Response({"message": "this operation is permited!"}, status.HTTP_403_FORBIDDEN)
