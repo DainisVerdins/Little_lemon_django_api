638dfb8704f105b21090847244f6d7891a4821c5
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2023-04-29 14:05:09 +0300

    feat: manager user views with all their logic

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -5,5 +5,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [ 
     path('menu-items', views.menu_items, name='menu-items'), 
     path('api-token-auth/', obtain_auth_token),
-    path('menu-items/<int:pk>', views.menu_item, name='menu-item')
+    path('menu-items/<int:menuItem>', views.menu_item, name='menu-item'),
+    path('groups/manager/users', views.managers_group_view, name='managers-group'),
+    path('groups/manager/users/<int:userId>', views.manager_view, name='managers-view')
 ] 
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import MenuItem, Category
-from .serializers import MenuItemSerializer
-from rest_framework import generics
+from .serializers import MenuItemSerializer, UserSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
+from django.contrib.auth.models import Group, User
 
 # Create your views here.
-@api_view(['GET','POST'])
+
+
+@api_view(['GET', 'POST'])
 def menu_items(request):
     """
     View list of menu items in the system.
@@ -18,23 +22,22 @@ def menu_items(request):
     if request.method == 'GET':
         items = MenuItem.objects.all()
         serialized_item = MenuItemSerializer(items, many=True)
-        
+
         return Response(serialized_item.data, status.HTTP_200_OK)
-    
+
     elif request.method == 'POST':
         if request.user.groups.filter(name="Manager").exists():
             title = request.POST.get('title')
             price = request.POST.get('price')
             featured = request.POST.get('featured')
             categoryId = request.POST.get('category')
-            
 
             category = Category.objects.get(pk=categoryId)
             menu_item = MenuItem(
-                title = title,
-                price = price,
-                featured = bool(featured),
-                category = category
+                title=title,
+                price=price,
+                featured=bool(featured),
+                category=category
             )
             try:
                 menu_item.save()
@@ -45,40 +48,90 @@ def menu_items(request):
         else:
             return Response({"message": "this operation is permited!"}, status.HTTP_403_FORBIDDEN)
 
-@api_view(['GET','PUT','PATCH','DELETE'])
-def menu_item(request, pk):
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def menu_item(request, menuItem):
 
     try:
-        item = MenuItem.objects.get(pk=pk)
+        item = MenuItem.objects.get(pk=menuItem)
     except MenuItem.DoesNotExist:
         return Response(status.HTTP_404_NOT_FOUND)
-    
+
     if request.method == 'GET':
         serialized_item = MenuItemSerializer(item, many=False)
-        
+
         return Response(serialized_item.data, status.HTTP_200_OK)
-    
+
     if request.method == 'PUT':
         if request.user.groups.filter(name="Manager").exists():
             serializer = MenuItemSerializer(item, data=request.data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(status.HTTP_200_OK)
-        
+
         return Response({"message": "this operation is permited!"}, status.HTTP_403_FORBIDDEN)
-    
+
     if request.method == 'PATCH':
         if request.user.groups.filter(name="Manager").exists():
             serializer = MenuItemSerializer(item, data=request.data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(status.HTTP_200_OK)
-        
+
         return Response({"message": "this operation is permited!"}, status.HTTP_403_FORBIDDEN)
-    
+
     if request.method == 'DELETE':
         if request.user.groups.filter(name="Manager").exists():
             item.delete()
             return Response(serialized_item.data, status.HTTP_200_OK)
-        
+
         return Response({"message": "this operation is permited!"}, status.HTTP_403_FORBIDDEN)
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def managers_group_view(request):
+    """
+        view for user with Manager role to assign or see all managers in system.
+
+    * [GET] Only Manager user can get a list of Managers in system.
+    * [POST] Only Manager could assign via POST method new Manager to system from payload
+    """
+    if request.user.groups.filter(name="Manager").exists():
+        if request.method == 'GET':
+            managers = User.objects.filter(groups__name='Manager')
+            serialized_item = UserSerializer(managers, many=True)
+
+            return Response(serialized_item.data, status.HTTP_200_OK)
+
+        if request.method == 'POST':
+            username = request.data['username']
+
+            if username:
+                user = get_object_or_404(User, username=username)
+                managers = Group.objects.get(name='Manager')
+                managers.user_set.add(user)
+                return Response(status.HTTP_201_CREATED)
+
+    else:
+        return Response({'message': 'this operation is permited!'}, status.HTTP_403_FORBIDDEN)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def manager_view(request,userId):
+    """
+    View to delete user with Manager role, only users with Manager role could do that.
+
+    * [DELETE] Deletes manager from the system.
+    """
+    if request.user.groups.filter(name="Manager").exists():
+        user = get_object_or_404(User, id=userId)
+        if request.method == 'DELETE':
+            managers = Group.objects.get(name='Manager')
+            managers.user_set.remove(user)
+
+            return Response(status.HTTP_200_OK)
+
+    else:
+        return Response({'message': 'this operation is permited!'}, status.HTTP_403_FORBIDDEN)
